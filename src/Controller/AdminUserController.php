a54b81cdac36f52ffac6f642aa53b90ccea87274
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-05-28 23:27:39 -0300

    fix: stub AdminUserController com construtor completo compatível com DI

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -4,16 +4,26 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Solicitacao;
+use App\Entity\User;
+use App\Message\EnviarEmailConta;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/admin/users-legacy', name: 'admin_users_legacy_')]
 class AdminUserController extends AbstractController
 {
     public function __construct(
-        private readonly UserRepository $userRepo,
+        private readonly EntityManagerInterface $em,
+        private readonly UserRepository         $userRepo,
+        private readonly MessageBusInterface    $bus,
+        private readonly LoggerInterface        $emailQueueLogger,
     ) {}
 
     #[Route('', name: 'index')]
